524c9bd33256cebc1e42620a618a1849dc67d0c5
Amelinda <amelindaa2705@gmail.com>
2024-10-22 13:58:17 +0700

    fixed routes bug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/",
     plugins: [react()],
+    build: {
+        outDir: "dist", // Ensure the correct output directory
+    },
+    server: {
+        open: true, // Opens the app in the browser automatically on dev
+    },
     resolve: {
         alias: {
             "react-router-dom": "react-router-dom",
