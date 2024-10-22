dbea942ad276e44d00f0d29cddc786377cb91b7b
Amelinda <amelindaa2705@gmail.com>
2024-10-22 13:50:57 +0700

    fixed routes bug

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      'react-router-dom': 'react-router-dom',
-      'lucide-react': 'lucide-react',
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "react-router-dom": "react-router-dom",
+            "lucide-react": "lucide-react",
+        },
+        server: {
+            historyApiFallback: true, // This enables SPA fallback in development mode
+        },
     },
-  },
-})
+});
