9690356585c0fe8d89e3d778be3bce68baa9a3fd
argapratama <argaprataman052@gmail.com>
2024-10-22 14:03:17 +0700

    add ChatBot

---
+++ src/pages/token-management/ChatBotPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainLayout from "../../layouts/MainLayout";
+import ChatBot from "../../components/ChatBot";
+
+const ChatBotPage = () => {
+    return (
+        <MainLayout>
+            <ChatBot />
+        </MainLayout>
+    )
+}
+
+export default ChatBotPage;
